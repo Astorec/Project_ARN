98b5c6640495410ba7a4ca3a504498e81c5fb321
Astorec <rossa1298@gmail.com>
2018-09-21 18:10:23 +0100

    Prototype_21_09_18_18_07
    Basic components to build up the prototype for the basis of the game. Movement has been added to the Character as well as the Coliders for the ground.
    
    Assets used:
    
    https://assetstore.unity.com/packages/2d/environments/free-platform-game-assets-85838

---
+++ Project ARN/Assets/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+
+    public Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb2d.MovePosition(rb2d.transform.position + tempVect);
+    }
+
+}
