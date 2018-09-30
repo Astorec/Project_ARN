42118d2357a76e0e85f1caf01ce5d3ef6203037e
Astorec <rossa1298@gmail.com>
2018-09-30 22:01:02 +0100

    30_09_18_22_00
    Made a new scene to try and see if I could get the movement working. Made some new discoveries whilst doing that

---
+++ Project ARN/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public float runSpeed = 40f;
+    public float jumpForce = 500f;
+    public Rigidbody2D rb2d;
+
+    float moveHorizontal = 0f;
+    float velocity = 0f;
+    bool jump = false;
+
+	// Use this for initialization
+	void Update () {
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(0, jumpForce));
+            jump = true;
+        }
+        
+        jump = false;
+	}
+}
