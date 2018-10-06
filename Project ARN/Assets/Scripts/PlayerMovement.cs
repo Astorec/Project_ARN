a2cf45900988d10c7a1b062c7db3f870107a4e90
Astorec <rossa1298@gmail.com>
2018-10-06 17:47:46 +0100

    ???
    Not sure what these are, it came up, ignore this one

---
+++ Project ARN/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D conrtoller;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    float horizontalMovement = 0f;
+
+	void Update () {
+        Debug.Log(jump);
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        conrtoller.Move(horizontalMovement * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+        crouch = true;
+    }
+}
