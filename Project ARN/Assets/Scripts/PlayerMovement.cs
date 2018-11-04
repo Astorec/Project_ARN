6fb14ead70dc028a6a0f31200e7aab67163dda18
Astorec <rossa1298@gmail.com>
2018-11-04 04:44:43 +0000

    Commit 04_11_18_04_43
    Added player animations and the movement is getting there. Need to add a way to get the player to stop moving when the key is released. JUmp physics need to be worked to let the player move in the air. The player is now flipping properly though

--- Project ARN/Assets/Scripts/PlayerMovement.cs
+++ Project ARN/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb2D;
     public Transform groundCheck;
     public LayerMask whatIsGround;
-
+   
     public float moveSpeed;
     public float jumpHeight;
     public float groundCheckRadius;
+    public float gravity;
 
     private bool grounded;
     private bool doubleJumped;
 
+    private Animator anim;
+    private SpriteRenderer sr;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
@@ -23,14 +33,32 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
 
-         if (Input.GetKey(KeyCode.A))
+         if (Input.GetKeyDown(KeyCode.A))
             rb2D.velocity = new Vector2(-moveSpeed, rb2D.velocity.y * Time.fixedDeltaTime);
         
-        if (Input.GetKey(KeyCode.D))
-            rb2D.velocity = new Vector2(moveSpeed, rb2D.velocity.y * Time.fixedDeltaTime);        
+        if (Input.GetKeyDown(KeyCode.D))
+            rb2D.velocity = new Vector2(moveSpeed, rb2D.velocity.y * Time.fixedDeltaTime);
+        
+        if (rb2D.velocity.x > 0)
+        {
+            sr.flipX = false;
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", true);
+        }
+            
+        else if (rb2D.velocity.x < 0)
+        {
+            sr.flipX = true;
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", true);
+        }
+            
 
         if (grounded)
             doubleJumped = false;
+
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Jump Height", rb2D.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
             Jump();
@@ -42,6 +70,8 @@ public class PlayerMovement : MonoBehaviour
             doubleJumped = true;
         }
 
+
+        anim.SetFloat("Speed", (rb2D.velocity.x));
        
     }
 
